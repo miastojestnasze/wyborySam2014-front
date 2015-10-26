18436612a7c0bdf4bf8fb3f414e84335e59a9752
Paweł Gałwa <pawelgalwa@Pawels-MacBook-Pro.local>
2015-10-26 17:50:26 +0100

    autoprefixer added to stylus

--- gulp_tasks/stylus.js
+++ gulp_tasks/stylus.js
@@ -1,19 +1,17 @@
 module.exports = stylus;
 
 function stylus(gulp, plugins) {
+  var autoprefixer = require('autoprefixer-stylus');
   return function() {
     gulp.src('./app/**/style.styl')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.stylus({
-      compress: true
+      compress: true,
+      use: [autoprefixer('iOS >= 7', 'last 1 Chrome version')]
     }))
     .pipe(plugins.sourcemaps.write())
     .pipe(plugins.rename({basename: 'app.min'}))
     .pipe(gulp.dest('build'));
-
-    // gulp.src('build/style.css')
-    // .pipe(plugins.rename({basename: 'test'}))
-    // .pipe(gulp.dest('./build'));
   }
 }
 
