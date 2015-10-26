15a46cf850570374db456d6aaa476c79e388396a
Paweł Gałwa <pawelgalwa@Pawels-MacBook-Pro.local>
2015-10-26 17:50:49 +0100

    angular-templates configured

--- gulp_tasks/angular-templates.js
+++ gulp_tasks/angular-templates.js
@@ -5,6 +5,7 @@ function angularTemplates(gulp, plugins) {
    gulp.src('./app/**/*.jade')
     .pipe(plugins.jade())
     .pipe(plugins.angularTemplatecache({
+      module: 'wyborySam2014',
       filename: 'templates.min.js'
     }))
     .pipe(plugins.uglify())
