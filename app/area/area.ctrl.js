678bcc39e25c77c9d8810ebf83bab72223e4a12f
Paweł Gałwa <pawelgalwa@Pawels-MacBook-Pro.local>
2015-11-01 20:58:06 +0100

    Prototype 2

--- app/area/area.ctrl.js
+++ app/area/area.ctrl.js
@@ -1,18 +1,48 @@
 angular.module('wyborySam2014.area')
 .controller('wyborySam2014.area.ctrl', ['$scope', function($scope){
-  var siteView = 'charts';
-  $scope.test = 21412;  
-  $scope.arr=[1,2,23,4]
-  $scope.switchView = function() {
-    if(siteView === 'charts') {
-      $('.tables-view').velocity("scroll", { axis: "x" });
-      siteView = 'tables';  
-    }
-    else if(siteView === 'tables') {
-      $('.charts-view').velocity("scroll", { axis: "x" });
-      siteView = 'charts'; 
-    }
-  }
+  $scope.menuIsVisible = function() {
+    return $scope.openMenu === false || $scope.openMenu === true;
+  };
+  $scope.menuIsOff = function() {
+    return $scope.openMenu === false;
+  };
 
+  $scope.electionKind = 'Wybory do Rady Miasta';
+  
+  $scope.dropdownIsOff = function() {
+    return $scope.electionKindsDropwn === false;
+  };
+
+  $scope.dropdownIsVisible = function() {
+    return $scope.electionKindsDropwn === false || $scope.electionKindsDropwn === true;
+  };
+
+  $scope.changeElectionKind = function(kind) {
+    $scope.electionKind = kind.name;
+    $scope.electionKindsDropwn = false;
+  };
+
+  
+  
+  $scope.districts = [
+    {name: 'Srodmiescie', id: 0},
+    {name: 'Ochota', id: 0},
+    {name: 'Wola', id: 0},
+    {name: 'Zoliborz', id: 0},
+    {name: 'Mokotow', id: 0},
+    {name: 'Ursus', id: 0},
+    {name: 'Ursynow', id: 0},
+    {name: 'Wilanow', id: 0},
+    {name: 'Bielany', id: 0},
+  ];
+
+  $scope.electionKinds = [
+    {name: 'Wybory do Rady Miasta', id:0},
+    {name: 'Wybory do Rady Dzielnicy', id:0},
+    {name: 'Wybory Prezydenta Miasta', id:0},
+    {name: 'Wybory Do Sejmiku', id:0}
+  ];
+
+  
   
 }]);
--- app/charts/main/charts.ctrl.js
+++ app/charts/main/charts.ctrl.js
@@ -46,14 +46,14 @@ angular.module('wyborySam2014.charts')
                     'margin': '10px 13px 0px 7px'
                 }
             },
-            caption: {
-                enable: true,
-                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
-                css: {
-                    'text-align': 'justify',
-                    'margin': '10px 13px 0px 7px'
-                }
-            }
+            // caption: {
+            //     enable: true,
+            //     // html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+            //     css: {
+            //         'text-align': 'justify',
+            //         'margin': '10px 13px 0px 7px'
+            //     }
+            // }
         };
 
         $scope.data = sinAndCos();
