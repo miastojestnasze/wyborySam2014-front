6068e8e86253a4a898a3839eab775132f8818675
Paweł Gałwa <pawelgalwa@Pawels-MacBook-Pro.local>
2015-11-11 23:37:33 +0100

    prototype 5 candidates added to statistics view

--- app/tables/main/tables.dir.js
+++ app/tables/main/tables.dir.js
@@ -2,12 +2,15 @@
 angular.module('wyborySam2014.tables')
 .directive('tablesView', [function(){
   return {
-    // scope: {}, // {} = isolate, true = child, false/undefined = no change
+    scope: {
+      stats: "=",
+      electionKind: "="
+      }, // {} = isolate, true = child, false/undefined = no change
     controller: 'wyborySam2014.tables.mainCtrl',
     restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment,
     templateUrl: 'tables/main/tables.dir.html',
-    link: function($scope, iElm, iAttrs, controller) {
-      
+    link: function(scope, iElm, iAttrs, controller) {
+      // scope.candidatesTableLimit = 30;
     }
   };
 }]);
