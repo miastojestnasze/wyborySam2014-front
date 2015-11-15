e8a41bec383f0b8828ef3b8e825a5e4e31d1e8dc
Paweł Gałwa <pawelgalwa@Pawels-MacBook-Pro.local>
2015-11-15 21:56:47 +0100

    landing page burned

--- app/config.js
+++ app/config.js
@@ -2,13 +2,14 @@ angular.module('wyborySam2014')
 .config(['$stateProvider', function($stateProvider) {
   $stateProvider
   .state('stats-app', {
-    url: '/app/{dataUrl}',
+    url: '/{dataUrl}',
     controller: 'wyborySam2014.area.ctrl',
     templateUrl: 'area/area.ctrl.html'
   })
   .state('landing', {
     url: '',
     controller: 'wyborySam2014.landing.ctrl',
-    templateUrl: 'landing_page/landing.ctrl.html'
+    templateUrl: 'area/area.ctrl.html',
+    deepStateRedirect: { default: { state: 'stats-app' } }
   })
 }])
