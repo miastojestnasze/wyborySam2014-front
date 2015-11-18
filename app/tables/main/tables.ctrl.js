e3d88b9a5f30ed7309350eeb0c06546e7aec1991
Łukasz Fiszer <lukasz.fiszer@gmail.com>
2015-11-18 11:18:08 +0100

    Fix pagination not working when changing search criterias

--- app/tables/main/tables.ctrl.js
+++ app/tables/main/tables.ctrl.js
@@ -1,9 +1,13 @@
 'use strict';
 angular.module('wyborySam2014.tables')
 .controller('wyborySam2014.tables.mainCtrl', ['$scope', function($scope){
-  $scope.rowCollection = [
-    {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-    {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-    {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
-  ];
+  
+  $scope.$watch('stats', function (newVal, oldVal) {
+    if (!newVal || !newVal.candidates) {
+      $scope.rawCandidates = [];
+      return;
+    }
+    $scope.rawCandidates = newVal.candidates;
+  });
+  
 }])
